006bce71726ccf81d34312ebb7856e1a7881cef1
Arifulislam5577 <arifalamm33@gmal.com>
2023-06-20 12:22:42 +0600

    updated: vue app updated

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,14 @@
 <template>
   <header class="py-6 bg-white">
-    <div class="container flex items-center justify-between">
-      <router-link to="/" class="font-bold text-black text-xl"
-        >Vue Router</router-link
-      >
+    <div
+      class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8 flex items-center justify-between"
+    >
+      <router-link to="/" class="font-bold text-black text-xl">
+        <img
+          class="w-auto h-9"
+          src="https://cdn.rareblocks.xyz/collection/celebration/images/logo.svg"
+          alt=""
+      /></router-link>
       <nav class="flex items-center gap-5 text-sm font-bold">
         <router-link to="/">Home</router-link>
         <router-link to="/about">About</router-link>
